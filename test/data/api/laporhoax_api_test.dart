ff09d5c37e3e2930e11a78499bfb480b37034817
Andi Irham <andi.irhamm@gmail.com>
2021-10-10 14:04:24 +0700

    Splash Screen and Icon

---
+++ test/data/api/laporhoax_api_test.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([Dio])
+void main() {
+  group('api test', () {
+    test('Return List of Restaurant', () {});
+  });
+}
