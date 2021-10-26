65ff3840d68a9eb8f20bb96086180e6a2df40152
Andi Irham <Andi.irhamm@gmail.com>
2021-10-26 11:27:28 +0700

    update tdd

--- test/presentation/provider/report_notifier_test.dart
+++ test/presentation/provider/report_notifier_test.dart
@@ -1,8 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:laporhoax/common/failure.dart';
 import 'package:laporhoax/common/state_enum.dart';
+import 'package:laporhoax/data/models/report_request.dart';
 import 'package:laporhoax/data/models/token_id.dart';
+import 'package:laporhoax/domain/entities/report.dart';
 import 'package:laporhoax/domain/usecases/delete_report.dart';
 import 'package:laporhoax/domain/usecases/get_categories.dart';
 import 'package:laporhoax/domain/usecases/get_reports.dart';
@@ -46,6 +49,30 @@ void main() {
 
   final tToken = 'token';
   final tId = 1;
+  final tReportRequest = ReportRequest(
+    user: 1,
+    url: "url",
+    category: "category",
+    isAnonym: false,
+    description: "description",
+    img: XFile('null'),
+  );
+  final tReport = Report(
+    id: 1,
+    url: "url",
+    img: "img",
+    category: "category",
+    status: "status",
+    isAnonym: false,
+    dateReported: "dateReported",
+    description: "description",
+    prosesDate: "prosesDate",
+    verdict: "verdict",
+    verdictDesc: "verdictDesc",
+    verdictDate: "verdictDate",
+    user: 1,
+    verdictJudge: 1,
+  );
 
   test('should change report data when data is gotten successfully', () async {
     // arrange
@@ -68,6 +95,81 @@ void main() {
     // assert
     expect(provider.fetchReportState, RequestState.Error);
     expect(provider.fetchReportMessage, "Can't get data");
+    expect(listenerCallCount, 2);
+  });
+
+  test('should change category data when data is gotten successfully', () async {
+    // arrange
+    when(mockGetCategories.execute())
+        .thenAnswer((_) async => Right([testCategory]));
+    // act
+    await provider.fetchCategories();
+    // assert
+    expect(provider.fetchCategoryState, RequestState.Loaded);
+    expect(provider.category, [testCategory]);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when fetch category data is unsuccessful', () async {
+    // arrange
+    when(mockGetCategories.execute())
+        .thenAnswer((_) async => Left(ServerFailure("")));
+    // act
+    await provider.fetchCategories();
+    // assert
+    expect(provider.fetchCategoryState, RequestState.Error);
+    expect(provider.fetchReportMessage, "");
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return callback when data sent successfully', () async {
+    // arrange
+    when(mockPostReport.execute(
+      tToken,
+      tReportRequest,
+    )).thenAnswer((_) async => Right(tReport));
+    // act
+    await provider.sendReport(tToken, tReportRequest);
+    // assert
+    expect(provider.postReportState, RequestState.Success);
+    expect(provider.report, tReport);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when sending is unsuccessful', () async {
+    // arrange
+    when(mockPostReport.execute(tToken, tReportRequest))
+        .thenAnswer((_) async => Left(ServerFailure("")));
+    // act
+    await provider.sendReport(tToken, tReportRequest);
+    // assert
+    expect(provider.postReportState, RequestState.Error);
+    expect(provider.fetchReportMessage, "");
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return callback when data remove sent successfully', () async {
+    // arrange
+    when(mockDeleteReport.execute(
+     tToken, tId,
+    )).thenAnswer((_) async => Right('Success'));
+    // act
+    await provider.removeReport(tToken, tId);
+    // assert
+    expect(provider.postReportState, RequestState.Success);
+    expect(provider.deleteReportMessage, 'Success');
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when sending remove is unsuccessful', () async {
+    // arrange
+    when(mockDeleteReport.execute(tToken, tId))
+        .thenAnswer((_) async => Left(ServerFailure("")));
+    // act
+    await provider.removeReport(tToken, tId);
+    // assert
+    expect(provider.postReportState, RequestState.Error);
+    expect(provider.deleteReportMessage, "");
     expect(listenerCallCount, 2);
   });
 }
