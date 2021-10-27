2073712d9f5a7928451320f2928c2759a142b9aa
Andi Irham <Andi.irhamm@gmail.com>
2021-10-27 20:02:15 +0700

    setup environment

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -6,6 +6,7 @@
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_app_check_web/firebase_app_check_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_messaging_web/firebase_messaging_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
@@ -19,6 +20,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseAppCheckWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
   FirebaseMessagingWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
