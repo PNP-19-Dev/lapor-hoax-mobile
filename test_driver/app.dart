b5665b233e37d67a284181e6a080959864d22df4
Andi Irham <Andi.irhamm@gmail.com>
2021-11-12 23:40:06 +0700

    Initializing Flutter Test Driver

---
+++ test_driver/app.dart
@@ -0,0 +1,13 @@
+/*
+ * Created by andii on 12/11/21 23.40
+ * Copyright (c) 2021 . All rights reserved.
+ * Last modified 12/11/21 23.39
+ */
+
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:laporhoax/main.dart' as app;
+
+void main() {
+  enableFlutterDriverExtension();
+  app.main();
+}
